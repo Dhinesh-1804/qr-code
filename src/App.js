9868591d987726ab8efe322d5bb2cba79d7fb8c9
Dhinesh-1804 <dhinesh06122002@gmail.com>
2024-11-06 18:26:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function QrCode(){
+    const [img,setImg]= useState("");
+    const [loading,setLoading]=useState(false);
+    const [qrData,setQrData]=useState("")
+    const [qrSize,setQrSize] =useState("")
+    async function generateQR(){
+        setLoading(true);
+        try {
+            const url = `https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}×${qrSize}&data=${encodeURIComponent(qrData)}`;
+            setImg(url);
+        } catch (error) {
+          console.error("Error generating QR code",error);
+        }finally{
+            setLoading(false);
+        }
+    }
+    function downloadQR(){
+        fetch(img).then((response) => response.blob()).then((blob) =>{
+            const link = document.createElement("a");
+            link.href=URL.createObjectURL(blob);
+            link.download="qrcode.png";
+            link.click();
+            document.body.removeChild(link);
+        }).catch((error)=>{
+            console.error("Error downloading QR",error)
+        })
+
+    }
+    return <div className="app-container">
+        <h1> QR CODE GENERATOR</h1>
+        {loading &&<p>Please wait...</p>}
+        {img && <img src={img} className="qr-code-image"/>}
+        <div>
+            <label htmlFor="dataInput" className="input-label">
+                Data For QR Code:
+            </label>
+            <input type="text" value={qrData} id="dataInput" placeholder="Enter Data for QR Code" onChange={(e)=>setQrData(e.target.value)}/>
+            <label htmlFor="sizeInput" className="input-label">
+                Image size (e.g.,150)
+            </label>
+            <input type="text" value={qrSize} onChange={(e)=>setQrSize(e.target.value)} id="sizeInput" placeholder="Enter image size"/>
+            <button className="generate-button" disabled={loading} onClick={generateQR}>Generate QR Code</button>
+            <button className="dowload-button" onClick={downloadQR}>Download QR Code</button>
+            
+        </div>
+        <h4 className="footer"> Designed By Dhinesh</h4>
+    
     </div>
-  );
 }
-
-export default App;
+export default QrCode ;
